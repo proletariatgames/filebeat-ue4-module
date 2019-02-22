ecd2ab7fd34a736d617c13348ab6f41074a78221
Dan Brakeley <dan@brakeley.net>
2019-02-22 15:32:51 -0500

    fix user/group problems when building on windows

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")"
 mkdir -p output
 rm -f ./output/ue4-module.tar.gz
 echo Generating ue4-module.tar.gz...
-tar -zcvf ./output/ue4-module.tar.gz module
+tar --owner=root --group=root -zcvf ./output/ue4-module.tar.gz module
 echo Done
